62d3c65c6d1035eb8d197f9290e1b778534f6e11
BharathyM <31205702+BharathyM@users.noreply.github.com>
2023-04-02 10:20:36 +0530

    Add files via upload
    day1

---
+++ dtypes.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class dtypes {
+
+	public static void main(String[] args) {
+		// code to demo Datatypes
+		
+		 int a=10;
+		 Boolean val= true;
+		 String stmt ="Printing values";
+		 
+		 System.out.println(a);
+		 System.out.println(val);
+		 System.out.println(stmt);
+
+	}
+
+}
